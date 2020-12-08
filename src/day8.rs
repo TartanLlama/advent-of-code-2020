7f679305e792ff716aeaeed32e80f0af2adb5609
Sy Brand <tartanllama@gmail.com>
2020-12-08 18:30:00 +0000

    Day8 better

--- src/day8.rs
+++ src/day8.rs
@@ -1,10 +1,12 @@
-use dynasmrt::{dynasm, DynasmApi, DynasmLabelApi, AssemblyOffset};
+use dynasmrt::{dynasm, relocations::Relocation, AssemblyOffset, DynasmApi, DynasmLabelApi};
 use winapi::um::memoryapi::VirtualProtect;
 use winapi::um::winnt::PAGE_EXECUTE_READWRITE;
 
 #[cfg(test)]
 mod tests {
     use std::ffi::c_void;
+
+    use dynasmrt::x64::X64Relocation;
 
     use super::*;
 
@@ -13,44 +15,53 @@ mod tests {
         let lines: Vec<_> = include_str!("../input/day8.txt").lines().collect();
 
         let mut ops = dynasmrt::x64::Assembler::new().unwrap();
-
-        dynasm!(ops
-        ; -> jmp_trampoline:
-        ; pop rdx
-        ; mov WORD [rdx - 6], WORD 0xcc
-        ; jmp rax);
         let code_start = ops.offset();
 
+        //rax will be our accumulator, so we need to zero it out
+        dynasm!(ops; xor rax, rax); 
+
+        //We'll mark the start of every source instruction with a label so we can jump between them
         let mut instr_labels: Vec<_> = (0..lines.len()).map(|_| ops.new_dynamic_label()).collect();
-        for (line_num, line) in lines.iter().enumerate() {            
+
+        for (line_num, line) in lines.iter().enumerate() {
             let (opcode, operand_str) = line.split_at(3);
             let operand: i32 = operand_str[1..].parse().unwrap();
-            
-            dynasm!(ops
-            ; =>instr_labels[line_num]);
+
+            //Output the label for this instruction
+            dynasm!(ops; =>instr_labels[line_num]);
             let loc = ops.offset();
+
+            //HERE BE DRAGONS
+            //This instruction modifies itself by scribbling over its opcode with 0xc3 (ret)
+            dynasm!(ops; mov DWORD [rip - 10], DWORD 0xc3);
+
+            //Now we JIT code for each source instruction
             match opcode {
-                "nop" => dynasm!(ops
-                    ; nop
-                    ; mov WORD [rip - 10], WORD 0xcc
-                ),
-                "acc" => dynasm!(ops
-                    ; add r15, DWORD operand
-                    ; mov WORD [rip - 16], WORD 0xcc
-                ),
-                "jmp" => dynasm!(ops
-                            ; lea rax, [=>instr_labels[(line_num as i32 + operand) as usize]]
-                            ; call ->jmp_trampoline),
+                "nop" => (), //Nops are free *does dance*
+                "acc" => dynasm!(ops; add rax, DWORD operand),
+                //Jump to the instruction using the label number calculated from the offset
+                "jmp" => dynasm!(ops; jmp =>instr_labels[(line_num as i32 + operand) as usize]),
                 _ => panic!("bad opcode"),
             }
         }
 
         let mut buf = ops.finalize().unwrap();
-        let mut old = 0;
+        
+        //We need to mark the memory for our code as readable, writeable and executable
         unsafe {
-            VirtualProtect((buf.ptr(AssemblyOffset(0)) as *mut c_void), buf.size(), PAGE_EXECUTE_READWRITE, &mut old);
+            let mut old = 0;
+            VirtualProtect(
+                (buf.ptr(AssemblyOffset(0)) as *mut c_void),
+                buf.size(),
+                PAGE_EXECUTE_READWRITE,
+                &mut old,
+            );
         }
-        let code_fn: extern "win64" fn() -> bool = unsafe { std::mem::transmute(buf.ptr(code_start)) };
-        code_fn();
+
+        //Cast our buffer into a function we can call
+        let find_solution: extern "win64" fn() -> i32 =
+            unsafe { std::mem::transmute(buf.ptr(code_start)) };
+
+        println!("Solution: {}", find_solution());
     }
 }
